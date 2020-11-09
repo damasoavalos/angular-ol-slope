16768e86239d0e5f0d02851e06087da510237189
Damaso <damasoavalos@gmail.com>
2020-11-08 19:06:34 -0700

    Added Angular-ol-slope project to repository

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import  Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import * as Proj from 'ol/proj';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  private map: Map;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.map = new Map({
+      layers: [
+        new Tile({ source: new OSM()}),
+      ],
+      target: 'map',
+      view: new View({
+        center: Proj.transform([-0.12755, 51.507222], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 3
+      })
+    });
+  }
+
+}
