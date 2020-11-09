17e05a86eaf6a8b93fbedda0c266ea13ff4f36c0
Damaso <damasoavalos@gmail.com>
2020-11-09 00:59:22 -0700

    Draw functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { OlmapComponent } from './olmap/olmap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    OlmapComponent
   ],
   imports: [
     BrowserModule,
