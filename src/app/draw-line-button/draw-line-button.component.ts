adced1dde32164f53c08f52b16ee72b940c16bf1
damasoavalos <damasoavalos@gmail.com>
2020-12-27 15:01:59 -0700

    "draw-line-button" moved it to html component

--- src/app/draw-line-button/draw-line-button.component.ts
+++ src/app/draw-line-button/draw-line-button.component.ts
@@ -1,6 +1,5 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component} from '@angular/core';
 import Control from 'ol/control/Control';
-// import GeometryType from 'ol/geom/GeometryType';
 
 @Component({
   selector: 'app-draw-line-button',
@@ -9,32 +8,37 @@ import Control from 'ol/control/Control';
 })
 
 export class DrawLineButtonComponent extends Control {
-  @Output() drawLine = new EventEmitter<{Line: string}>();
+  // @Output() drawLine = new EventEmitter<{Line: string}>();
+  button: HTMLElement;
+  element: HTMLElement;
+
   constructor() {
       super({});
-      const button = document.createElement('button');
-      button.type = 'button';
-      button.className = 'ol-draw-line';
-      button.innerHTML = 'Polygon';
-      button.style.position = 'absolute';
-      button.style.borderRadius = '2px 2px 0 0';
-      button.style.width = '4.5em';
-      button.style.left = '.5em';
-      button.style.top = '4em';
-      button.style.margin = '1';
-      const element = document.createElement('div');
-      element.className = 'ol-control';
-      element.appendChild(button);
+      this.button = document.getElementById('draw-line-button');
+      this.element = document.getElementById('ol-draw-line');
+      // const button = document.createElement('button');
+      // button.type = 'button';
+      // button.className = 'draw-line-button';
+      // button.innerHTML = 'Polygon';
+      // button.style.position = 'absolute';
+      // button.style.borderRadius = '2px 2px 0 0';
+      // button.style.width = '4.5em';
+      // button.style.left = '.5em';
+      // button.style.top = '4em';
+      // button.style.margin = '1';
+      // const element = document.createElement('div');
+      // element.className = 'ol-control';
+      // element.appendChild(button);
       Control.call(this, {
-          element
+          element: this.element
       });
-      button.addEventListener('click', () => this.click());
+      // this.button.addEventListener('click', () => this.click());
   }
 
-  click(): void {
+  onclick(): void {
       console.log('click');
-      this.drawLine.emit({
-        Line: 'line'
-      });
+      // this.drawLine.emit({
+      //   Line: 'line'
+      // });
   }
 }
