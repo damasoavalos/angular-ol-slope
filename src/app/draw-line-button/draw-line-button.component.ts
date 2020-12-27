3d55fc636f4c7be748ce6f2307245bbe94e517fd
damasoavalos <damasoavalos@gmail.com>
2020-12-27 02:00:35 -0700

    minor issues fixed

--- src/app/draw-line-button/draw-line-button.component.ts
+++ src/app/draw-line-button/draw-line-button.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
 import Control from 'ol/control/Control';
 
 @Component({
@@ -6,27 +6,28 @@ import Control from 'ol/control/Control';
   templateUrl: './draw-line-button.component.html',
   styleUrls: ['./draw-line-button.component.css']
 })
+
 export class DrawLineButtonComponent extends Control {
   constructor() {
       super({});
-      // const button = document.createElement('button');
-      // button.type = 'button';
-      // button.className = 'ol-draw-line';
-      // button.innerHTML = 'Polygon';
-      // // button.style.position = 'absolute';
-      // // button.style.borderRadius = '2px 2px 0 0';
-      // // button.style.width = '4.5em';
-      // // button.style.left = '.5em';
-      // // button.style.top = '4em';
-      // const element = document.createElement('div');
-      // element.className = 'ol-control';
-      // // element.style.width = 'auto';
-      // // element.style.position = 'relative';
-      // element.appendChild(button);
-      // Control.call(this, {
-      //     element
-      // });
-      // button.addEventListener('click', () => this.click());
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.className = 'ol-draw-line';
+      button.innerHTML = 'Polygon';
+      button.style.position = 'absolute';
+      button.style.borderRadius = '2px 2px 0 0';
+      button.style.width = '4.5em';
+      button.style.left = '.5em';
+      button.style.top = '4em';
+      const element = document.createElement('div');
+      element.className = 'ol-control';
+      // element.style.width = 'auto';
+      // element.style.position = 'relative';
+      element.appendChild(button);
+      Control.call(this, {
+          element
+      });
+      button.addEventListener('click', () => this.click());
   }
 
   click(): void {
