6fa7468036451de0ec84f5cdb2ecce16f6bad5f0
damasoavalos <damasoavalos@gmail.com>
2020-12-28 05:00:14 -0700

    "draw line", "draw polygon" and "delete" functionality added

--- src/app/ol-map/ol-map.component.ts
+++ src/app/ol-map/ol-map.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit} from '@angular/core';
 import 'ol/ol.css';
 import Draw from 'ol/interaction/Draw';
-import {defaults as defaultInteractions} from 'ol/interaction';
+import {defaults as defaultInteractions, Select} from 'ol/interaction';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import GeometryType from 'ol/geom/GeometryType';
 import {defaults as defaultControls} from 'ol/control';
+import ContextMenu from 'ol-contextmenu';
 import { DrawLineButtonComponent } from '../draw-line-button/draw-line-button.component';
 import {DrawPolygonButtonComponent} from '../draw-polygon-button/draw-polygon-button.component';
 import { DrawInteractionService} from '../draw-interaction.service';
 import {Subscription} from 'rxjs';
-import * as OLUtil from 'ol/util';
 
 @Component({
   selector: 'app-ol-map',
@@ -25,14 +25,15 @@ export class OlMapComponent implements OnInit {
   map: Map;
   source: VectorSource;
   draw: Draw;
+  vector: VectorLayer;
+  select: Select;
+  featureToDelete: any;
 
   constructor(private drawInteractionService: DrawInteractionService) {
     this.clickDrawLineEventSubscription = this.drawInteractionService.getClickDrawLine().subscribe(() => {
-      this.RemoveInteraction();
       this.addInteraction(GeometryType.LINE_STRING);
     });
     this.clickDrawPolygonEventSubscription = this.drawInteractionService.getClickDrawPolygon().subscribe(() => {
-      this.RemoveInteraction();
       this.addInteraction(GeometryType.POLYGON);
     });
   }
@@ -44,32 +45,59 @@ export class OlMapComponent implements OnInit {
 
     this.source = new VectorSource({wrapX: false});
 
-    const vector = new VectorLayer({
+    this.vector = new VectorLayer({
       source: this.source,
     });
 
+    this.select = new Select();
+    const deleteIcon = 'https://cdn.iconscout.com/icon/free/png-32/delete-726-458722.png';
+    const contextmenuItems = [{
+      text: 'Delete',
+      classname: 'marker',
+      icon: deleteIcon,
+      callback: (() => {
+          this.source.removeFeature(this.featureToDelete);
+      })
+    },
+      '-' // this is a separator
+    ];
+
+    const contextmenu = new ContextMenu({
+      width: 180,
+      defaultItems: true,
+      items: contextmenuItems
+    });
     const mapControls = [new DrawLineButtonComponent(new DrawInteractionService()),
-                         new DrawPolygonButtonComponent(new DrawInteractionService())];
+                         new DrawPolygonButtonComponent(new DrawInteractionService()),
+                         contextmenu];
 
     this.map = new Map({
-      interactions: defaultInteractions({ doubleClickZoom: false }),
+      interactions: defaultInteractions({ doubleClickZoom: false }).extend([this.select]),
       controls: defaultControls().extend(mapControls),
-      layers: [basemap, vector],
+      layers: [basemap, this.vector],
       target: 'map',
       view: new View({
         center: [-11000000, 4600000],
         zoom: 4,
       }),
     });
-    console.log('OpenLayers Version: ', OLUtil.VERSION);
-    // vector.getSource().on('addfeature', (event) => {
-    //   this.RemoveInteraction();
-    // });
+
+    contextmenu.on('beforeopen', (evt) => {
+      const feature = this.map.forEachFeatureAtPixel(evt.pixel, (ft) => {
+        return ft;
+      });
+      this.featureToDelete = feature;
+      if (feature) { // open only on features
+        contextmenu.enable();
+      } else {
+        contextmenu.disable();
+      }
+    });
   }
 
-  RemoveInteraction(): void {
-    this.map.removeInteraction(this.draw);
-  }
+  // RemoveInteraction(): void {
+  //   this.map.removeInteraction(this.draw);
+  // }
 
   addInteraction(geometryType): void {
     this.draw = new Draw({
